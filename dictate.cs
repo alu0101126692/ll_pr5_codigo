574082d51612d66b8e3c8e0ef175317fcc995d05
alu0101126692 <56158185+alu0101126692@users.noreply.github.com>
2021-11-19 17:33:02 +0000

    Add files via upload

---
+++ dictate.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+using TMPro;
+
+public class dictate : MonoBehaviour
+{
+
+
+    private DictationRecognizer dictado;
+    public bool dictando;
+    public keyword reconocedorKeyword;
+    public TextMeshPro texto;
+
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        administarDictado();
+    }
+
+
+    private void empezarDictado() {
+
+        dictado = new DictationRecognizer();
+
+        dictado.DictationResult += (text, confidence) => {
+
+            Debug.Log("Resultado reconocimiento " + text);
+            texto.text = text;
+
+        };
+
+        dictado.DictationHypothesis += (text) => {
+
+            Debug.Log("Hipotesis del texto " + text);
+            texto.text = text + " (Hipotesis)";
+
+        };
+
+
+    }
+    private void administarDictado() {
+        if (reconocedorKeyword.encendido) { // Solo ejecutar esto si esta encedido
+            if (!dictando && !reconocedorKeyword.reconociendo) {
+                if (Input.GetKeyDown("g")) {
+                    empezarDictado();
+                    dictando = true;
+                    Debug.Log(PhraseRecognitionSystem.Status);
+                    if (PhraseRecognitionSystem.Status.ToString() != "Stopped")
+                        PhraseRecognitionSystem.Shutdown(); // Tiene que apagarse esto antes de cada ejecucion ya que no puede ejecutarse a la vez que el dictado
+                    
+                    
+                    //reconocedorKeyword.parar();
+                    dictado.Start();
+                } 
+
+                
+            } else if (dictando){
+                if (Input.GetKeyDown("g")) {
+                    dictando = false;
+                    dictado.Stop();
+                    parar();
+                } 
+            }
+        }
+    }
+
+    public void parar() {
+        if (dictado != null)
+            dictado.Dispose();
+    }
+}
---
+++ keyword.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class keyword : MonoBehaviour
+{
+    public string[] keywords;
+    private KeywordRecognizer recognizer;
+    public bool reconociendo;
+    public dictate reconocedorDictado;
+
+    public bool encendido;
+
+    public Material materialApagado;
+    public Material materialEncendido;
+    public Material materialRed;
+
+
+    public Material materialRandom;
+
+    public Renderer materialActual;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        administarReconocedor();
+        
+    }
+
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args) {
+        Debug.Log ("Keyword: " + args.text + " Confianza: " + args.confidence + " Momento en el que se inicio: " + args.phraseStartTime + " Duracion: "  + args.phraseDuration);
+
+
+
+        if (encendido) {
+            switch (args.text) {
+                case "Apagar":
+                
+                    encendido = false;
+                    materialActual.material = materialApagado;
+
+                    if (reconocedorDictado.texto.text.Length != 0) {
+                        reconocedorDictado.texto.text = "";
+                    }
+                
+                break;
+
+                case "Cambiar Canal":
+
+                        materialRandom.color = new Color(Random.Range(0f, 1f),Random.Range(0f, 1f),Random.Range(0f, 1f));
+                        materialActual.material = materialRandom;
+                    break;
+                case "Canal 1": 
+
+                        materialActual.material = materialEncendido;
+                    break;
+                
+                case "Canal 2": 
+
+                        materialActual.material = materialRed;
+                    break;
+                default:
+                    Debug.Log(args.text + " no es una orden");
+                break;
+                
+            }
+        } else if (args.text == "Encender") {
+            materialActual.material = materialEncendido;
+            encendido = true;
+        }
+        
+            
+            
+
+        
+    
+    }
+
+
+    private void empezarKeyword() {
+        recognizer = new KeywordRecognizer(keywords);
+
+        recognizer.OnPhraseRecognized += OnPhraseRecognized;
+    }
+    private void administarReconocedor() {
+        if (!reconociendo && !reconocedorDictado.dictando) {
+            if (Input.GetKeyDown("f")) {
+                empezarKeyword();
+                reconociendo = true;
+                //reconocedorDictado.parar();
+
+                recognizer.Start();
+            } 
+
+            
+        } else if (reconociendo){
+            if (Input.GetKeyDown("f")) {
+                reconociendo = false;
+                recognizer.Stop();
+                parar();
+            } 
+        }
+    }
+    
+    public void parar() {
+        recognizer.Dispose();
+    }
+}
